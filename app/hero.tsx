727098e44790116e058bc143d7c5181efa2326ad
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-12-30 23:23:40 +0600

    Update

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { JSX } from 'react'
+import Link from 'next/link'
 import Typewriter from '@/app/_component/typewriter'
-import Link from 'next/link';
 
 
 /**
@@ -15,7 +15,6 @@ export default function Hero(): JSX.Element {
 	return (
 		<>
 			{/*<ParticleAnimation/>*/ }
-			{ /* Animated hero component for an AI company using tailwind css */ }
 			<div
 				className="absolute inset-0 h-full w-full bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30">
 			</div>
@@ -41,7 +40,7 @@ export default function Hero(): JSX.Element {
 					style={ { clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' } }></div>
 			</div>
 			{/* Background animation */ }
-			<div className="absolute inset-0 h-full w-full bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30">
+			<div className="absolute inset-0 h-full min-h-screen w-full bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30">
 				<svg className="absolute inset-0 h-full w-full" preserveAspectRatio="xMidYMid slice"
 				     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1463 678">
 					<path
@@ -64,7 +63,7 @@ export default function Hero(): JSX.Element {
 				</svg>
 			</div>
 			{/* Content */ }
-			<div className="relative">
+			<div className="relative h-full min-h-screen">
 				<div className="max-w-7xl mx-auto px-4 sm:px-6">
 					<div className="pt-32 pb-12 md:pt-40 md:pb-20">
 						<div className="max-w-4xl mx-auto text-center">
@@ -72,7 +71,7 @@ export default function Hero(): JSX.Element {
 								className="text-5xl md:text-6xl font-extrabold text-white tracking-tight sm:text-7xl">
 								<span className="block text-gray-800 xl:inline">We build </span>
 								<span
-									className="block bg-indigo-600 text-white xl:inline">
+									className="inline bg-indigo-600 text-white">
 									S<Typewriter content="oftware"/>
 								</span>
 								<span className="block text-gray-800 xl:inline"> for </span>
@@ -80,36 +79,34 @@ export default function Hero(): JSX.Element {
 							</h1>
 							<p className="mt-3 text-base text-gray-900 sm:mt-5 sm:text-xl lg:text-2xl">
 								<strong>
-									We are a team of AI experts, software engineers, and business professionals who are
-									passionate about building AI solutions for businesses.
+									We are a team of software engineers, hardware engineers, AI experts, and designers who are
+									passionate about building technology solutions for you.
 								</strong>
 							</p>
 							<div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
 								<div
 									className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-									<a href="#"
+									<Link href="/server"
 									   className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-purple-600 hover:bg-purple-700 sm:px-8">
 										Get Server
-									</a>
-									<a href="#"
-									   className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-gray-800 bg-white hover:bg-gray-200 sm:px-8">
-										Develop Software
-									</a>
+									</Link>
+									<Link href="/web-development"
+									   className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-black bg-white hover:bg-gray-200 sm:px-8">
+										Website Development
+									</Link>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="relative">
-						{/* 4 grid to show services */ }
-						<div className="hidden md:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-10">
-							{/* Note: Overlay layers block Select buttons */ }
+						<div className="hidden md:grid grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
 							<div
 								className="bg-white shadow-md rounded-lg px-10 py-6 bg-opacity-30 hover:bg-opacity-100">
 								<h2 className="text-2xl font-bold text-center text-gray-900">
-									Domain
+									Domain Name Registration
 								</h2>
 								<p className="mt-4 text-center text-gray-800">
-									We provide domain registration service for your business.
+									We provide domain registration service.
 								</p>
 								<div className="mt-6">
 									<Link href="/domain"
@@ -121,25 +118,25 @@ export default function Hero(): JSX.Element {
 							<div
 								className="bg-white shadow-md rounded-lg px-10 py-6 bg-opacity-30 hover:bg-opacity-100">
 								<h2 className="text-2xl font-bold text-center text-gray-900">
-									Hosting
+									Server and Hosting
 								</h2>
 								<p className="mt-4 text-center text-gray-800">
-									We provide hosting service for your business.
+									We provide server and hosting service.
 								</p>
 								<div className="mt-6">
 									<Link href="/hosting"
 									      className="block button w-full bg-purple-600 text-white text-center font-bold py-2 px-4 rounded hover:bg-purple-700 focus:outline-none focus:bg-indigo-700">
-										Hosting
+										Server
 									</Link>
 								</div>
 							</div>
 							<div
 								className="bg-white shadow-md rounded-lg px-10 py-6 bg-opacity-30 hover:bg-opacity-100">
 								<h2 className="text-2xl font-bold text-center text-gray-900">
-									Software
+									Software Development
 								</h2>
 								<p className="mt-4 text-center text-gray-800">
-									We provide software development service for your business.
+									We provide software development service.
 								</p>
 								<div className="mt-6">
 									<Link href="/software"
@@ -151,45 +148,15 @@ export default function Hero(): JSX.Element {
 							<div
 								className="bg-white shadow-md rounded-lg px-10 py-6 bg-opacity-30 hover:bg-opacity-100">
 								<h2 className="text-2xl font-bold text-center text-gray-900">
-									AI
+									Artificial Intelligence
 								</h2>
 								<p className="mt-4 text-center text-gray-800">
-									We provide AI service for your business.
+									We provide AI development solution.
 								</p>
 								<div className="mt-6">
 									<Link href="/ai"
 									      className="block button w-full bg-purple-600 text-white text-center font-bold py-2 px-4 rounded hover:bg-purple-700 focus:outline-none focus:bg-indigo-700">
-										AI
-									</Link>
-								</div>
-							</div>
-							<div
-								className="bg-white shadow-md rounded-lg px-10 py-6 bg-opacity-30 hover:bg-opacity-100">
-								<h2 className="text-2xl font-bold text-center text-gray-900">
-									SEO
-								</h2>
-								<p className="mt-4 text-center text-gray-800">
-									We provide SEO service for your business.
-								</p>
-								<div className="mt-6">
-									<Link href="/seo"
-									      className="block button w-full bg-purple-600 text-white text-center font-bold py-2 px-4 rounded hover:bg-purple-700 focus:outline-none focus:bg-indigo-700">
-										SEO
-									</Link>
-								</div>
-							</div>
-							<div
-								className="bg-white shadow-md rounded-lg px-10 py-6 bg-opacity-30 hover:bg-opacity-100">
-								<h2 className="text-2xl font-bold text-center text-gray-900">
-									Marketing
-								</h2>
-								<p className="mt-4 text-center text-gray-800">
-									We provide marketing service for your business.
-								</p>
-								<div className="mt-6">
-									<Link href="/marketing"
-									      className="block button w-full bg-purple-600 text-white text-center font-bold py-2 px-4 rounded hover:bg-purple-700 focus:outline-none focus:bg-indigo-700">
-										Marketing
+										Artificial Intelligence
 									</Link>
 								</div>
 							</div>
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,169 @@ export default function Page(): JSX.Element {
 	return (
 		<>
 			<Hero/>
-			<div className="bg-white">
+			<div className="relative isolate px-6 lg:px-8">
+				<div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+				     aria-hidden="true">
+					<div
+						className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+						style={ { clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' } }>
+					</div>
+				</div>
+				<div
+					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+					aria-hidden="true">
+					<div
+						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+						style={ { clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' } }></div>
+				</div>
+				<div
+					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+					aria-hidden="true">
+					<div
+						className="relative left-[calc(50%-3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+						style={ { clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' } }></div>
+				</div>
+				<div className="md:px-20 py-10">
+					<h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+						Web Hosting Packages
+					</h2>
+					<p className="mt-4 text-xl text-gray-500">
+						We are providing web hosting since 2015 with customer satisfaction.
+					</p>
+					<div className="mt-10">
+						{/* Pricing Table */}
+						<div className="bg-white rounded-lg shadow overflow-hidden divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-3 sm:gap-px">
+							<div
+								className="relative group bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+								<div>
+									<span className="rounded-lg inline-flex p-3 bg-white text-[#ff80b5] ring-4 ring-white">
+										{/* Heroicon name: globe-alt */ }
+										<svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+										     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+											      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"/>
+										</svg>
+									</span>
+								</div>
+								<h3 className="mt-8 text-lg font-medium text-white">
+									Free
+								</h3>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> Website
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> GB SSD Storage
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">10</span> GB Bandwidth
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> Email Account
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> Subdomain
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> Database
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> FTP Account
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> Cronjob
+								</p>
+								<p className="mt-2 text-sm text-white">
+									<span className="font-bold">1</span> SSL Certificate
+								</p>
+							</div>
+							<div className="relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+								<div>
+									<span className="rounded-lg inline-flex p-3 bg-[#ff80b5] text-white">
+										{/* Heroicon name: scale */ }
+										<svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+										     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+											      d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m6 2l-6 2"/>
+										</svg>
+									</span>
+								</div>
+								<h3 className="mt-8 text-lg font-medium text-gray-900">
+									Standard
+								</h3>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">1</span> Website
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> GB SSD Storage
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> GB Bandwidth
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> Email Account
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> Subdomain
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> Database
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> FTP Account
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> Cronjob
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> SSL Certificate
+								</p>
+							</div>
+							<div className="relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+								<div>
+									<span className="rounded-lg inline-flex p-3 bg-[#ff80b5] text-white">
+										{/* Heroicon name: lightning-bolt */ }
+										<svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+										     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+											      d="M13 10V3L4 14h7v7l9-11h-7z"/>
+										</svg>
+									</span>
+								</div>
+								<h3 className="mt-8 text-lg font-medium text-gray-900">
+									Premium
+								</h3>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">10</span> Website
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> GB SSD Storage
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">1000</span> GB Bandwidth
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> Email Account
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> Subdomain
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> Database
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> FTP Account
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> Cronjob
+								</p>
+								<p className="mt-2 text-sm text-gray-500">
+									<span className="font-bold">100</span> SSL Certificate
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="px-20 py-10"></div>
 				<div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
 					<div className="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
 						<div>
@@ -152,6 +314,7 @@ export default function Page(): JSX.Element {
 						</div>
 					</div>
 				</div>
+				<div className="px-20 py-10"></div>
 			</div>
 		</>
 	)
