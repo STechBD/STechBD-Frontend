23781bbf17d6c76a054fc73feeadd3c8ccf939a8
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2024-03-10 01:00:15 +0600

    Update

--- app/_component/testimonial.tsx
+++ app/_component/testimonial.tsx
@@ -1,8 +1,8 @@
 import { JSX } from 'react'
 import Image from 'next/image'
 import Markdown from 'react-markdown'
+import remarkEmoji from 'remark-emoji'
 import rehypeRaw from 'rehype-raw'
-import Link from 'next/link';
 
 
 /**
@@ -33,6 +33,57 @@ export default function Testimonial(): JSX.Element {
 
 	return (
 		<>
+			{/*{ reviews.map((review, index) => (
+				<div key={ index }
+				     className="flex flex-col rounded-lg shadow-sm hover:shadow-xl overflow-hidden bg-white dark:bg-gray-800">
+					<div className="flex-shrink-0">
+						<Image className="h-48 w-full object-cover"
+						       src={ review.image }
+						       alt={ review.name }
+						       width={ 384 }
+						       height={ 384 }
+						/>
+					</div>
+					<div className="flex-1 p-6 flex flex-col justify-between">
+						<div className="flex-1">
+							<p className="text-sm font-medium text-primary dark:text-gray-200">
+								{ review.organization } • { review.profession }
+							</p>
+							<p className="mt-2 text-xl font-bold text-gray-900 dark:text-white">
+								{ review.name }
+							</p>
+							<p className="mt-2">
+								{ [ ...Array(review.rating) ].map((_, index) => (
+									<svg key={ index } className="h-5 w-5 inline text-primary dark:text-blue-500"
+									     fill="currentColor"
+									     viewBox="0 0 20 20"
+									     xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M10 3.05l2.3 6.65H18l-5.65 4.1 2.3 6.65L10 14.85l-5.65 4.1 2.3-6.65L2 9.7h5.7L10 3.05z"/>
+									</svg>
+								)) }
+								{ [ ...Array(5 - review.rating) ].map((_, index) => (
+									<svg key={ index } className="h-5 w-5 inline text-gray-300 dark:text-gray-600"
+									     fill="currentColor"
+									     viewBox="0 0 20 20"
+									     xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M10 3.05l2.3 6.65H18l-5.65 4.1 2.3 6.65L10 14.85l-5.65 4.1 2.3-6.65L2 9.7h5.7L10 3.05z"/>
+									</svg>
+								)) }
+							</p>
+							<div className="mt-2 text-lg text-gray-500 dark:text-gray-400">
+								<Markdown rehypePlugins={ [
+									rehypeRaw,
+								] }
+								>
+									{ review.review }
+								</Markdown>
+							</div>
+						</div>
+					</div>
+				</div>
+			)) }*/}
 			{ reviews.map((review, index) => (
 				<div key={ index }
 				     className="flex flex-col rounded-lg shadow-lg hover:shadow-xl overflow-hidden bg-white dark:bg-gray-800"
@@ -63,7 +114,8 @@ export default function Testimonial(): JSX.Element {
 									<svg key={ index } className="h-5 w-5 inline text-primary dark:text-blue-500"
 									     fill="currentColor"
 									     viewBox="0 0 20 20"
-									     xmlns="http://www.w3.org/2000/svg">
+									     xmlns="http://www.w3.org/2000/svg"
+									>
 										<path
 											d="M10 3.05l2.3 6.65H18l-5.65 4.1 2.3 6.65L10 14.85l-5.65 4.1 2.3-6.65L2 9.7h5.7L10 3.05z"/>
 									</svg>
@@ -79,9 +131,13 @@ export default function Testimonial(): JSX.Element {
 								)) }
 							</p>
 							<div className="mt-2 text-lg text-gray-500 dark:text-gray-400">
-								<Markdown rehypePlugins={ [
-									rehypeRaw,
-								] }
+								<Markdown
+									remarkPlugins={ [
+										remarkEmoji,
+									] }
+									rehypePlugins={ [
+										rehypeRaw,
+									] }
 								>
 									{ review.review }
 								</Markdown>
